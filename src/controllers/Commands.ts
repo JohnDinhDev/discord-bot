6ff82d7b4b49ab5465e1b2627e5d1a3a17ace050
John Dinh <johnduydinh@gmail.com>
2021-02-19 14:04:45 -0500

    fix PREFIX env value

--- src/controllers/Commands.ts
+++ src/controllers/Commands.ts
@@ -1,47 +1,48 @@
 // Definitions
-import { Msg, Keyable } from "../definitions/interfaces";
+import { Msg, Keyable } from '../definitions/interfaces'
 
 // Views
-import Message from "../views/Message";
-import Embed from "../views/Embed";
+import Message from '../views/Message'
+import Embed from '../views/Embed'
 
 export default class Commands {
     // TODO: make env type for prefix, and make tests for empty env config
-    private readonly prefix: string = process.env.PREFIX!;
+    private readonly prefix: string = process.env.DISCORD_PREFIX!;
     private args: Array<string>;
     private command: string | undefined;
     private msg: Message;
 
-    constructor(msg: Msg) {
-        // Set properties
-        this.msg = new Message(msg);
+    constructor (msg: Msg) {
+      // Set properties
+      this.msg = new Message(msg)
 
-        // Parses message content for a command and arguments
-        this.args = msg.content.slice(this.prefix.length).trim().split(" ");
-        this.command = this.args.shift()?.toLowerCase();
+      // Parses message content for a command and arguments
+      this.args = msg.content.slice(this.prefix.length).trim().split(' ')
+      this.command = this.args.shift()?.toLowerCase()
 
-        if (!msg.content.startsWith(this.prefix) || !this.command) return;
+      console.log(this.args, this.command, this.prefix)
+      if (!msg.content.startsWith(this.prefix) || !this.command) return
 
-        // Run method if input command exists, else run help
-        if (this.command in this) {
-            (this as Keyable)[this.command]();
-        } else {
-            this.help();
-        }
+      // Run method if input command exists, else run help
+      if (this.command in this) {
+        (this as Keyable)[this.command]()
+      } else {
+        this.help()
+      }
     }
 
     private help = async (): Promise<void> => {
-        // If user input command is invalid
-        if ((this.command || "") in this === false) {
-            this.msg.reply(
-                "`" + this.command + "`" + " is not a valid command"
-            );
-        }
+      // If user input command is invalid
+      if ((this.command || '') in this === false) {
+        this.msg.reply(
+          '`' + this.command + '`' + ' is not a valid command'
+        )
+      }
 
-        // Generate help embed
-        const embed = Embed.help();
+      // Generate help embed
+      const embed = Embed.help()
 
-        // Send embed message
-        this.msg.send(embed);
+      // Send embed message
+      this.msg.send(embed)
     };
 }
--- src/views/Embed.ts
+++ src/views/Embed.ts
@@ -1,57 +1,57 @@
-import Text from "./Text";
-import { Commands } from "../definitions/enums";
-import { Keyable } from "../definitions/interfaces";
-import { generateLineBreaks } from "../utils";
+import Text from './Text'
+import { Commands } from '../definitions/enums'
+import { Keyable } from '../definitions/interfaces'
+import { generateLineBreaks } from '../utils'
 export default class Embed {
     public static generic = (
-        color: string,
-        title: string,
-        description: string
+      color: string,
+      title: string,
+      description: string
     ) => {
-        return {
-            embed: {
-                color,
-                title,
-                description,
-            },
-        };
+      return {
+        embed: {
+          color,
+          title,
+          description
+        }
+      }
     };
 
     public static pinnedMessage = () => {
-        const [title, description] = Text.pinnedMessage();
+      const [title, description] = Text.pinnedMessage()
 
-        return {
-            embed: {
-                color: 0x3f66ff,
-                title,
-                description,
-            },
-        };
+      return {
+        embed: {
+          color: 0x3f66ff,
+          title,
+          description
+        }
+      }
     };
 
     public static help = () => {
-        const message = [];
+      const message = []
 
-        for (const command of Object.keys(Commands)) {
-            message.push(
-                `\`${process.env.PREFIX + command + " "}\` - ${
+      for (const command of Object.keys(Commands)) {
+        message.push(
+                `\`${process.env.DISCORD_PREFIX + command + ' '}\` - ${
                     (Commands as Keyable)[command]
                 }`
-            );
-        }
+        )
+      }
 
-        return {
-            embed: {
-                color: 0xff7f50,
-                title: "Help",
-                fields: [
-                    {
-                        name: "Commands",
-                        value: generateLineBreaks(message, false, 2),
-                        inline: false,
-                    },
-                ],
-            },
-        };
+      return {
+        embed: {
+          color: 0xff7f50,
+          title: 'Help',
+          fields: [
+            {
+              name: 'Commands',
+              value: generateLineBreaks(message, false, 2),
+              inline: false
+            }
+          ]
+        }
+      }
     };
 }
