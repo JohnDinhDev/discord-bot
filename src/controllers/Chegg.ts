1ccb594ecdcf61afca0fc2df794be66baa9f6c12
aaronfeinberg <afeinberg35@gmail.com>
2021-02-19 20:16:27 -0500

    testing puppeteer

--- src/controllers/Chegg.ts
+++ src/controllers/Chegg.ts
@@ -1,12 +1,21 @@
+//Puppeteer (scraper)
+import puppeteer from 'puppeteer'
+
 export default class Chegg {
 
   constructor() {
+    //var chegg_session=this.startPuppeteer(); 
+  }
+
 
   }
 
   /*
    *  exampleFunction = () => {
-   *
-   *  }
+   *   
+   *  }  
    */
-}
+
+    //request
+
+  }
